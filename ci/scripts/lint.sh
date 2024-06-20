d1dec63c2676ba709faf3317403e5703891e014c
oluwawunmi <soluwawunmi@gmail.com>
2024-06-20 13:08:03 +0100

    test-ci

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eux
 
-pushd dp-search-data-extractor
+pushd dp-frontend-search-controller
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.57.2
+  go install github.com/kevinburke/go-bindata/go-bindata
   make lint
 popd
