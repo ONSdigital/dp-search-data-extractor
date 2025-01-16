ae5be6cd3c0e81798bf132c468ff75f9a888e21a
cookel2 <eleanor.cook@ons.gov.uk>
2025-01-16 16:42:19 +0000

    extract function to do search data import mapping for the handler

--- handler/search_content_handler.go
+++ handler/search_content_handler.go
@@ -44,34 +44,7 @@ func (h *SearchContentHandler) Handle(ctx context.Context, _ int, msg kafka.Mess
 }
 
 func (h *SearchContentHandler) sendSearchDataImported(ctx context.Context, resource models.SearchContentUpdate) error {
-	searchDataImport := models.SearchDataImport{
-		URI:             resource.URI,
-		Title:           resource.Title,
-		DataType:        resource.ContentType,
-		Summary:         resource.Summary,
-		Survey:          resource.Survey,
-		MetaDescription: resource.MetaDescription,
-		ReleaseDate:     resource.ReleaseDate,
-		Language:        resource.Language,
-		Edition:         resource.Edition,
-		DatasetID:       resource.DatasetID,
-		CDID:            resource.CDID,
-		CanonicalTopic:  resource.CanonicalTopic,
-		Topics:          []string{},
-	}
-
-	// Assign topics only if they're not nil
-	if resource.Topics != nil {
-		searchDataImport.Topics = resource.Topics
-	}
-
-	if resource.ContentType == ReleaseDataType {
-		searchDataImport.Cancelled = resource.Cancelled
-		searchDataImport.Finalised = resource.Finalised
-		searchDataImport.Published = resource.Published
-		searchDataImport.ProvisionalDate = resource.ProvisionalDate
-		searchDataImport.DateChanges = resource.DateChanges
-	}
+	searchDataImport := models.MapResourceToSearchDataImport(resource)
 
 	// Marshall Avro and sending message
 	if err := h.Producer.Send(schema.SearchDataImportEvent, searchDataImport); err != nil {
---
+++ models/mapper_resource.go
@@ -0,0 +1,34 @@
+package models
+
+// MapResourceToSearchDataImport Performs default mapping of a SearchContentUpdate struct to a SearchDataImport struct.
+func MapResourceToSearchDataImport(resource SearchContentUpdate) SearchDataImport {
+	searchDataImport := SearchDataImport{
+		URI:             resource.URI,
+		Title:           resource.Title,
+		DataType:        resource.ContentType,
+		Summary:         resource.Summary,
+		Survey:          resource.Survey,
+		MetaDescription: resource.MetaDescription,
+		ReleaseDate:     resource.ReleaseDate,
+		Language:        resource.Language,
+		Edition:         resource.Edition,
+		DatasetID:       resource.DatasetID,
+		CDID:            resource.CDID,
+		CanonicalTopic:  resource.CanonicalTopic,
+		Topics:          []string{},
+	}
+
+	// Assign topics only if they're not nil
+	if resource.Topics != nil {
+		searchDataImport.Topics = resource.Topics
+	}
+
+	if resource.ContentType == ReleaseDataType {
+		searchDataImport.Cancelled = resource.Cancelled
+		searchDataImport.Finalised = resource.Finalised
+		searchDataImport.Published = resource.Published
+		searchDataImport.ProvisionalDate = resource.ProvisionalDate
+		searchDataImport.DateChanges = resource.DateChanges
+	}
+	return searchDataImport
+}
